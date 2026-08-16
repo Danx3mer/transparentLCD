d74963def619ea123fef3a1f881f94d8cc63a5d6
Danx3mer <dashingdaniel05@gmail.com>
2026-08-16 01:48:10 -0400

    feat: implement UART via 2 Arduino Unos

--- transparentLCD.ino
+++ transparentLCD.ino
@@ -24,9 +24,10 @@ U8G2_ST7565_64128N_F_4W_HW_SPI u8g2(U8G2_R2, /*cs*/ 10, /*dc*/ 8, /*reset*/ 9);
 constexpr unsigned short DISPLAY_W = 128;
 constexpr unsigned short DISPLAY_H = 64;
 
-constexpr unsigned short refreshRateMS = 100;
+constexpr unsigned short refreshRateMS = 50;
 
 void setup(void) {
+  Serial.begin(9600);
   u8g2.begin();  // begin function is required for u8g2
   u8g2.setFont(u8g2_font_6x10_tf);
   u8g2.setFontRefHeightAll();  	/* this will add some extra space for the text inside the buttons */
@@ -35,19 +36,10 @@ void setup(void) {
 
 // Main drawing loop
 void drawFrame(int frameToDraw) {
-  static unsigned short x = 0, y = 24;
-  static short dirX = 1, dirY = 1;
-  static constexpr unsigned short dims = 16;
-
-  if(x + dirX + dims > DISPLAY_W) dirX = -1;
-  else if(x + dirX == 0) dirX = 1;
-  x += dirX;
-
-  if(y + dirY + dims > DISPLAY_H) dirY = -1;
-  else if(y + dirY == 0) dirY = 1;
-  y += dirY;
-
-  u8g2.drawXBMP(x, y, 16, 16, epd_bitmap_694_LR);
+  static int val = 0;
+  if(Serial.available()) val = Serial.read();
+  u8g2.drawStr(15, 15, String(val * 347).c_str());
+  Serial.println(val);
 }
 
 void loop(void) {
